becbea3800c9d869460bd5d81a8a16bb7d2410dd
IntegerLimit <103940576+IntegerLimit@users.noreply.github.com>
2023-07-05 15:50:49 +1000

    Item Borders Compat

--- src/main/java/com/integerlimit/deprecatedapi/integration/top/TOP.java
+++ src/main/java/com/integerlimit/deprecatedapi/integration/top/TOP.java
@@ -1,11 +1,36 @@
 package com.integerlimit.deprecatedapi.integration.top;
 
+import com.integerlimit.deprecatedapi.Tags;
+import com.integerlimit.deprecatedapi.deprecation.DeprecatedBlock;
+import com.integerlimit.deprecatedapi.deprecation.DeprecatedBlocks;
 import mcjty.theoneprobe.TheOneProbe;
-import mcjty.theoneprobe.api.ITheOneProbe;
+import mcjty.theoneprobe.api.*;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 
 public class TOP {
     public static void registerProviders() {
         ITheOneProbe TOP = TheOneProbe.theOneProbeImp;
         TOP.registerProvider(new TOPDeprecatedWarning());
+    }
+
+    public static class TOPDeprecatedWarning implements IProbeInfoProvider {
+        @Override
+        public String getID() {
+            return Tags.MODID + "top_deprecated_warning";
+        }
+
+        @Override
+        public void addProbeInfo(ProbeMode probeMode, IProbeInfo probeInfo, EntityPlayer player, World world, IBlockState blockState, IProbeHitData probeHitData) {
+            Block sourceBlock = blockState.getBlock();
+            DeprecatedBlock block = DeprecatedBlocks.getBlock(sourceBlock.getRegistryName(), sourceBlock.getMetaFromState(blockState));
+            if (block != null) {
+                String text = TextFormatting.RED + IProbeInfo.STARTLOC + block.getTOPWailaMessage() + IProbeInfo.ENDLOC;
+                probeInfo.text(text);
+            }
+        }
     }
 }
