0f8d781974a4c53a9c648f1f2cecce61278be5b7
IntegerLimit <103940576+IntegerLimit@users.noreply.github.com>
2023-08-28 21:32:54 +1000

    Fix crash on server

--- src/main/java/com/integerlimit/deprecatedapi/EventHandler.java
+++ src/main/java/com/integerlimit/deprecatedapi/EventHandler.java
@@ -7,11 +7,14 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class EventHandler {
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     @SuppressWarnings("unused")
+    @SideOnly(Side.CLIENT)
     public void tooltipEvent(ItemTooltipEvent event) {
         ItemStack itemStack = event.getItemStack();
         DeprecatedItem item = DeprecatedItems.getItem(itemStack);
